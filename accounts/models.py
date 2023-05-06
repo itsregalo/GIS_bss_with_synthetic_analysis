b3990e299b6f775025ad51d9f51b40fb66144077
itsregalo <itsregalo047@gmail.com>
2023-05-06 23:32:05 +0300

    included accounts module

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-app_name = 'accounts'
-
 # Create your models here.
+
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True)
-    address = models.CharField(max_length=100, blank=True)
-    city = models.CharField(max_length=50, blank=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    # login with email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
 
     def __str__(self):
         return self.username
     
-
-    
-
+    class Meta:
+        verbose_name_plural = 'Users'
+        db_table = 'users'
+        
