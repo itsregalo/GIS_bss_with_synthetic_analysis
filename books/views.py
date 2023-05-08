2dd3908e97f293f684d9f0cc0aa703fdaac1d063
itsregalo <itsregalo047@gmail.com>
2023-05-08 03:47:35 +0300

    implemented geodata

--- books/views.py
+++ books/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from .models import *
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
+from django.contrib.auth.decorators import login_required
 
 radius = 20
 
 # Create your views here.
 
+@login_required
 def book_listing(request, *args, **kwargs):
+    book_owner = BookOwner.objects.get(user=request.user)
     nearby_books = Book.objects.annotate(distance=Distance('location', request.user.bookowner.location)).order_by('distance')[0:10]
     context = {
-        'nearby_books': nearby_books
+        'nearby_books': nearby_books,
+        'book_owner': book_owner
     }
     return render(request, 'books_listing.html', context)
 
