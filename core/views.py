098d242d7058f9f8c6d7bd179da011c4c3f249e9
itsregalo <itsregalo047@gmail.com>
2023-05-09 04:16:53 +0300

    order by location

--- core/views.py
+++ core/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from books.models import Book, BookOwner
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
 
 # Create your views here.
+radius = 20
 
 def index(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    ip = request.META.get('REMOTE_ADDR')
-    print(ip)
-    print(x_forwarded_for)
-    return render(request, 'index.html')
+    latest_books = Book.objects.order_by('-timestamp')[:8]
+    if request.user.is_authenticated:
+        book_owner = BookOwner.objects.get(user=request.user)
+        nearby_books = Book.objects.annotate(distance=Distance('location', book_owner.location)).order_by('distance')[0:10]
+    else:
+        # choose random 6 books
+        nearby_books = Book.objects.order_by('?')[:6]
+    context = {
+        'latest_books': latest_books,
+        'nearby_books': nearby_books,
+    }
+    return render(request, 'index.html', context)
